79e103699ac7a5ae2f8168d576ec70d20eb151ac
Damilola91 <damilola.wiwoloku@gmail.com>
2025-08-24 20:00:22 +0200

    traduzione Payment Section

--- src/components/PaymentSection/PaymentSection.jsx
+++ src/components/PaymentSection/PaymentSection.jsx
@@ -2,27 +2,32 @@
 
 import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "../PaymentForm/PaymentForm";
+import { useTranslation } from "react-i18next";
 
 const PaymentSection = ({
   stripePromise,
   scrollToUserForm,
   bookingId,
   onPaymentSuccess,
-}) => (
-  <section className="mb-12 max-w-lg mx-auto">
-    <h2 className="text-2xl font-semibold mb-4 text-[#46331d] text-center">
-      Pagamento
-    </h2>
-    <div className="shadow-md rounded-md">
-      <Elements stripe={stripePromise}>
-        <PaymentForm
-          scrollToUserForm={scrollToUserForm}
-          bookingId={bookingId}
-          onPaymentSuccess={onPaymentSuccess}
-        />
-      </Elements>
-    </div>
-  </section>
-);
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <section className="mb-12 max-w-lg mx-auto">
+      <h2 className="text-2xl font-semibold mb-4 text-[#46331d] text-center">
+        {t("paymentSection.title")}
+      </h2>
+      <div className="shadow-md rounded-md">
+        <Elements stripe={stripePromise}>
+          <PaymentForm
+            scrollToUserForm={scrollToUserForm}
+            bookingId={bookingId}
+            onPaymentSuccess={onPaymentSuccess}
+          />
+        </Elements>
+      </div>
+    </section>
+  );
+};
 
 export default PaymentSection;
