c7a47568017163b456cd8f9ea5171554b4f65f79
Damilola91 <damilola.wiwoloku@gmail.com>
2025-10-11 19:37:43 +0200

    timer

--- src/components/GlobalLoader/GlobalLoader.jsx
+++ src/components/GlobalLoader/GlobalLoader.jsx
@@ -53,7 +53,7 @@ const GlobalLoader = () => {
       setPrevPath(pathname);
     } else {
       // Delay per far vedere il loader anche su caricamenti rapidi
-      timer = setTimeout(() => setIsVisible(false), 1200);
+      timer = setTimeout(() => setIsVisible(false), 1000);
     }
     return () => clearTimeout(timer);
   }, [loading, pathname]);
